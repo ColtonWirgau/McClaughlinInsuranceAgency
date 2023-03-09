60e5d28b286c786147e8d51c661273ec111ceea1
Colton Wirgau <cwirgau@wbc-pc780.localdomain>
2023-03-09 10:46:45 -0500

    v0.24

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,17 +11,19 @@ function Home() {
 			<section>
 				<Products />
 			</section>
-			<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-				<input type="hidden" name="form-name" value="contact" />
-				<label for="insuranceType">I am interested in</label>
-				<select name="insuranceType" id="insuranceType" required>
-					<option value="personal">Whole Life Insurance</option>
-					<option value="business">Term Life Insurance</option>
-					<option value="business">Supplemental Life Insurance</option>
-					<option value="other">Other</option>
-				</select>
-				<input type="submit" className="btn margin-top" value="submit"></input>
-			</form>
+			<section>
+				<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+					<input type="hidden" name="form-name" value="contact" />
+					<label for="insuranceType">I am interested in</label>
+					<select name="insuranceType" id="insuranceType" required>
+						<option value="personal">Whole Life Insurance</option>
+						<option value="business">Term Life Insurance</option>
+						<option value="business">Supplemental Life Insurance</option>
+						<option value="other">Other</option>
+					</select>
+					<input type="submit" className="btn margin-top" value="submit"></input>
+				</form>
+			</section>
 		</>
 	);
 }
